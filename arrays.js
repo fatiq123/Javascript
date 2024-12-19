f00bc80cc011f720ed98689a7ddf77ffa03cb884
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2024-12-19 12:25:44 +0500

    arrays in javascript

--- arrays.js
+++ arrays.js
@@ -8,3 +8,33 @@ console.log(beforeFruits, fruits);
 
 
 console.log(typeof (arr));
+
+
+
+
+
+
+// Array methods
+console.log('\n');
+console.log('=============== Array Methods==============');
+
+// Foreach
+let sweets = ['chocolate', 'candy', 'jaggery', 'lemon'];
+sweets.forEach(n => console.log(n));
+
+console.log('\n');
+// Filter
+const filteredSweets = sweets.filter(s => s.startsWith('c'));
+console.log(filteredSweets);
+
+console.log('\n');
+// Slice
+const slicedArray = sweets.slice(1,3); // include 1 and 2 index only
+console.log(slicedArray);
+
+
+console.log('\n');
+// Map
+const mapedArray = sweets.map(m => m.toUpperCase()); // include 1 and 2 index only
+console.log(mapedArray);
+
